42a2db9fc39ce8a9243389c1a0ad999d651d519e
unknown <iguser@VSERVERHUMANTAB.engage.sap>
2014-06-16 19:26:02 +0100

    Revert "Limit the waiting in time."
    This reverts commit 96500da9079dd4382aab491045f5c08ea2071793.

--- src/ofxCEFBrowser.cpp
+++ src/ofxCEFBrowser.cpp
@@ -88,8 +88,8 @@ void ofxCEFBrowser::loadTex(ofPixels * texture) {
 	mPixels = texture;
 	mLoadIntoPixels = true;
 	mLoadedTexture = false;
-	auto t1 = time(NULL);
-	while ((mBrowser->IsLoading() || !mLoadedTexture || !mPixels->isAllocated()) && (difftime(time(NULL), t1) < 2)) {
+
+	while (mBrowser->IsLoading() || !mLoadedTexture || !mPixels->isAllocated()) {
 		CefDoMessageLoopWork();
 	}
 }
